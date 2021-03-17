9004d3aaf4a412146919c007c98cb2a3351729c4
1ncursio <ckswn1323@naver.com>
2021-03-17 20:33:38 +0900

    [UPDATE] youtube download route

--- server/index.js
+++ server/index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const ytdl = require('ytdl-core');
 
 const URL = 'https://www.youtube.com/watch?v=gSXFn4Ai6CQ';
 
-app.get('/download/:url', (req, res, next) => {
-  const { url } = req.params;
+app.use(cors());
 
-  res.json({ url });
+app.get('/download', (req, res, next) => {
+  const { url } = req.query;
+
+  res.header('Content-Disposition', 'attachment; filename="video.mp4');
+
+  ytdl(url, { format: 'mp4' }).pipe(res);
 });
 
 const PORT = process.env.PORT || 5100;
