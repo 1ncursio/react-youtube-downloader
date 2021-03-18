2b8edd3437ee32c61844cdb82291968958d02b65
1ncursio <ckswn1323@naver.com>
2021-03-18 20:06:01 +0900

    [UPDATE] duration format

--- client/pages/Video/index.tsx
+++ client/pages/Video/index.tsx
@@ -1,8 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import queryString from 'query-string';
 import useSWR from 'swr';
 import fetcher from '@utils/fetcher';
 import { IVideo } from '@typings/IVideo';
+// @ts-ignore
+import TimeFormat from 'hh-mm-ss';
 
 const Video = () => {
   const { url } = queryString.parse(location.search);
@@ -13,15 +15,28 @@ const Video = () => {
     console.log(videoData);
   }, [videoData]);
 
+  const convertSeconds = useCallback(
+    (sec: string) => {
+      if (!videoData) return;
+
+      const s = parseInt(sec, 10);
+
+      if (s >= 3600) return TimeFormat.fromS(s, 'hh:mm:ss');
+      return TimeFormat.fromS(s, 'mm:ss');
+    },
+    [videoData]
+  );
+
   return (
     <>
       {videoData && (
         <>
           <h1>{videoData.title}</h1>
           <h2>{videoData.author.name}</h2>
-          <span>{videoData.lengthSeconds}</span>
+          <span>{convertSeconds(videoData.lengthSeconds)}</span>
+          <span>{}</span>
           <img src={videoData.thumbnails[3].url} />
-          <a href={`http://localhost:3095/api/download?url=${url}`} target="_blank">
+          <a href={`http://localhost:3095/api/download?url=${url}`} target="_blank" rel="noopener noreferrer">
             다운로드
           </a>
         </>
